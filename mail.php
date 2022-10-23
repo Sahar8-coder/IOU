9b419e6dd705134be9e084090baf80784db96a3e
Sahar8-coder <ze.kirilov@gmail.com>
2022-10-23 20:52:10 +0300

    s

--- mail.php
+++ mail.php
@@ -11,7 +11,7 @@ $email = $_POST['user_email'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'ze.kirilov@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = '815@.LJKC'; // Ваш пароль от почты с которой будут отправляться письма
