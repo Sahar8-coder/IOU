011d802b69496dd2f3cf248abf05ca9c1c0ba997
Sahar8-coder <ze.kirilov@gmail.com>
2022-10-24 17:41:11 +0300

    v

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const nodemailer = require('nodemailer')
+
+let testEmailAccount = await nodemailer.createTestAccount()
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'ze.kirilov@gmail.com',
+      pass: '815@.LJKC',
+    },
+  })
+
+let result = await transporter.sendMail({
+  from: '"Node js" <ze.kirilov@gmail.com>',
+  to: 'ze.kirilov@gmail.com, ze.kirilov@gmail.com',
+  subject: 'Message from Node js',
+  text: 'This message was sent from Node js server.',
+  html:
+    'This <i>message</i> was sent from <strong>Node js</strong> server.',
+})
+
+console.log(result)
