3513c3fbdb07045f41d2a2a54805ac187ca9badc
Sahar8-coder <ze.kirilov@gmail.com>
2022-10-24 17:52:49 +0300

    d

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 const nodemailer = require('nodemailer')
 
-let testEmailAccount = await nodemailer.createTestAccount()
-
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -10,13 +8,17 @@ let transporter = nodemailer.createTransport({
     },
   })
 
-let result = await transporter.sendMail({
-  from: '"Node js" <ze.kirilov@gmail.com>',
-  to: 'ze.kirilov@gmail.com, ze.kirilov@gmail.com',
-  subject: 'Message from Node js',
-  text: 'This message was sent from Node js server.',
-  html:
-    'This <i>message</i> was sent from <strong>Node js</strong> server.',
-})
 
-console.log(result)
+
+document.getElementById('button').onclick = async function() {
+    alert("button was clicked");
+    await transporter.sendMail({
+        from: '"Node js" <ze.kirilov@gmail.com>',
+        to: 'ze.kirilov@gmail.com, ze.kirilov@gmail.com',
+        subject: 'Message from Node js',
+        text: 'This message was sent from Node js server.',
+        html:
+          'This <i>message</i> was sent from <strong>Node js</strong> server.',
+      })
+ }
+ console.log(result)
